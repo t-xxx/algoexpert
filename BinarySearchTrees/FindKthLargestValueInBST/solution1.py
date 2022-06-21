2b491eb1d35f1e946475e06c298ca69a98db4531
t-xxx <happycatcat23@gmail.com>
2022-06-21 23:59:34 +0900

    Added solutions

---
+++ BinarySearchTrees/FindKthLargestValueInBST/solution1.py
@@ -0,0 +1,28 @@
+class BST:
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+
+class TreeInfo:
+    def __init__(self, numberOfNodesVisited, latestVisitedNodeValue):
+        self.numberOfNodesVisited = numberOfNodesVisited
+        self.latestVisitedNodeValue = latestVisitedNodeValue
+        
+
+def findKthLargestValueInBst(tree, k):
+    treeInfo = TreeInfo(0, -1)
+    reverseInOrderTraverse(tree, k, treeInfo)
+    return treeInfo.latestVisitedNodeValue
+
+
+def reverseInOrderTraverse(node, k, treeInfo):
+	if node is None or treeInfo.numberOfNodesVisited >= k:
+		return
+	
+	reverseInOrderTraverse(node.right, k, treeInfo)
+	if treeInfo.numberOfNodesVisited < k:
+		treeInfo.numberOfNodesVisited += 1
+		treeInfo.latestVisitedNodeValue = node.value
+		reverseInOrderTraverse(node.left, k , treeInfo)
