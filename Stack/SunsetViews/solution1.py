37ce87355e8e485f1abb83369a9ca8d3f043d2f7
t-xxx <happycatcat23@gmail.com>
2022-07-02 01:46:19 +0900

    Added solutions

---
+++ Stack/SunsetViews/solution1.py
@@ -0,0 +1,24 @@
+def sunsetViews(buildings, direction):
+    candidateBuildings = []
+
+    startIdx = 0 if direction == "EAST" else len(buildings) - 1
+    step = 1 if direction == "EAST" else -1
+
+    idx = startIdx
+    while idx >= 0 and idx < len(buildings):
+        buildingHeight = buildings[idx]
+
+        while len(candidateBuildings) > 0 and \
+                buildings[candidateBuildings[-1]] <= buildingHeight:
+            candidateBuildings.pop()
+
+        candidateBuildings.append(idx)
+
+        idx += step
+
+    if direction == "WEST":
+        return candidateBuildings[::-1]
+
+    print(candidateBuildings)
+
+    return candidateBuildings
